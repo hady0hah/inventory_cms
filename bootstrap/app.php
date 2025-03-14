57d0610f0debac5dc68b52f29d00186ee5f4ee1b
hady43 <hadi.hawy@layoutinternational.com>
2025-03-14 21:57:53 +0200

    Fixed issue in middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(append: [
+        $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
