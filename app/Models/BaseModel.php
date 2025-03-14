fcb1787280cce57a5cdda0784f908bb2130eeb76
hady43 <hadi.hawy@layoutinternational.com>
2025-03-14 15:33:43 +0200

    Added Base Model class for all models

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'published'
+    ];
+
+    protected $attributes = [
+        'published' => true, // Default value for the published field
+    ];
+
+    protected $casts = [
+        'published' => 'boolean', // Cast the published field to boolean
+    ];
+
+
+}
